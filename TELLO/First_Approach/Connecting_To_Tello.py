69050948804fdaad97dcd7194b3b4fddf14c5511
rodrigo-pacheco <rpacheco95@gmail.com>
2019-04-01 22:26:27 +0200

    Basic UDP server (prints messages received) and client (send two strings) up and running. Information added for TELLO programming and supporting useful information

--- TELLO/First_Approach/Connecting_To_Tello.py
+++ TELLO/First_Approach/Connecting_To_Tello.py
@@ -8,6 +8,15 @@ Message = "Hello, Server"
 # Socket creation
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Sending information
+# Sending information (1)
 clientSock.sendto(Message.encode(encoding='UTF-8'), (UDP_IP_ADDRESS, UDP_PORT_NO))
+
+# Receiving information
+data = clientSock.recv(4096)
+print("Received: " + data.decode(encoding='UTF-8'))
+
+# Sending information (2)
 clientSock.sendto("Bye Bye, Server".encode(encoding='UTF-8'), (UDP_IP_ADDRESS, UDP_PORT_NO))
+# Receiving information
+data = clientSock.recv(4096)
+print("Received: " + data.decode(encoding='UTF-8'))
--- TELLO/First_Approach/Silly_Server.py
+++ TELLO/First_Approach/Silly_Server.py
@@ -3,6 +3,7 @@ import socket
 # Constants
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 6789
+RESPONSE = "This is a basic server. We are communicating. You sent me: "
 
 # Create server socket
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,4 +14,6 @@ serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
 # Print messages received
 while True:
     data, addr = serverSock.recvfrom(1024)
-    print ("Message: " + data.decode(encoding='UTF-8'))
+    if data:
+        print ("Message: " + data.decode(encoding='UTF-8'))
+        serverSock.sendto(RESPONSE.encode(encoding='UTF-8') + data, addr)
